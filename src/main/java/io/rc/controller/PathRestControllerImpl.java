a038100454f03d6c51336643ae109e74b8f54ade
Raffaele Camanzo <raffaele.camanzo@gmail.com>
2017-06-18 17:39:31 +0200

    Removed boilerplate annotations (Swagger can successfully read from generated interfaces)

--- src/main/java/io/rc/controller/PathRestControllerImpl.java
+++ src/main/java/io/rc/controller/PathRestControllerImpl.java
@@ -4,10 +4,6 @@ import io.rc.service.MyResourceService;
 import io.rc.simple.spring.boot.rest.api.PathRestController;
 import io.rc.simple.spring.boot.rest.model.Error;
 import io.rc.simple.spring.boot.rest.model.MyResource;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -38,17 +34,9 @@ public class PathRestControllerImpl implements PathRestController {
 		return headers;
 	}
 
-	@Override
 	@RequestMapping(value="/path/to/resources/{resource_id}",method=RequestMethod.DELETE,consumes={ "application/json" } , produces={ "application/json" })
-	@ApiOperation(value = "Delete a resource", tags = { "PathsAndMethodsAvailable" }, notes = "Delete the resource with the given id ", response = MyResource.class)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Resource deleted (get it if you need it!)"),
-			@ApiResponse(code = 404, message = "Resource not found"),
-			@ApiResponse(code = 500, message = "Unexpected error")
-	})
 	public ResponseEntity deleteResource(
 			// "resourceId"
-			@ApiParam(value = "The resource id.", required=true )
 			@PathVariable("resource_id")
 					String resourceId) {
 
@@ -65,16 +53,9 @@ public class PathRestControllerImpl implements PathRestController {
 		return new ResponseEntity(error, getHeaders(), HttpStatus.BAD_REQUEST);
 	}
 
-	@Override
 	@RequestMapping(value="/path/to/resources",method=RequestMethod.GET , produces={ "application/json" })
-	@ApiOperation(value = "Get the resources", tags = { "PathsAndMethodsAvailable" }, notes = "A way to retrieve the resources ", response = MyResource.class)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Everything ok!"),
-			@ApiResponse(code = 500, message = "Unexpected error")
-	})
 	public ResponseEntity getAllTheResources(
 			// "meaningfulQueryParam"
-			@ApiParam(value = "something which makes sense in your domain")
 			@RequestParam(value="meaningful_query_param", required=false)
 					String meaningfulQueryParam) {
 
@@ -82,17 +63,9 @@ public class PathRestControllerImpl implements PathRestController {
 				getHeaders(), HttpStatus.OK);
 	}
 
-	@Override
 	@RequestMapping(value="/path/to/resources/{resource_id}",method=RequestMethod.GET , produces={ "application/json" })
-	@ApiOperation(value = "Get a specific resource", tags = { "PathsAndMethodsAvailable" }, notes = "The resource i want. ", response = MyResource.class)
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Everything ok!"),
-			@ApiResponse(code = 404, message = "Resource not found"),
-			@ApiResponse(code = 500, message = "Unexpected error")
-	})
 	public ResponseEntity getTheResourcesWithId(
 			// "resourceId"
-			@ApiParam(value = "The resource id.", required=true )
 			@PathVariable("resource_id")
 					String resourceId) {
 
@@ -109,33 +82,18 @@ public class PathRestControllerImpl implements PathRestController {
 		return new ResponseEntity(error, getHeaders(), HttpStatus.BAD_REQUEST);
 	}
 
-	@Override
 	@RequestMapping(value="/path/to/resources",method=RequestMethod.HEAD,consumes={ "application/json" } , produces={ "application/json" })
-	@ApiOperation(value = "Get the resources header", tags = { "PathsAndMethodsAvailable" }, notes = "My resources header ")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Everything ok!"),
-			@ApiResponse(code = 500, message = "Unexpected error")
-	})
 	public ResponseEntity headForAllTheResources(
 			// "meaningfulQueryParam"
-			@ApiParam(value = "something which makes sense in your domain")
 			@RequestParam(value="meaningful_query_param", required=false)
 					String meaningfulQueryParam) {
 
 		return new ResponseEntity(null, getHeaders(), HttpStatus.OK);
 	}
 
-	@Override
 	@RequestMapping(value="/path/to/resources",method=RequestMethod.POST,consumes={ "application/json" } , produces={ "application/json" })
-	@ApiOperation(value = "Add a new resource", tags = { "PathsAndMethodsAvailable" }, notes = "Add a new resource to the list of resources ")
-	@ApiResponses(value = {
-			@ApiResponse(code = 201, message = "Resource inserted!"),
-			@ApiResponse(code = 400, message = "Wrong payload"),
-			@ApiResponse(code = 500, message = "Unexpected error")
-	})
 	public ResponseEntity insertNewResource(
 			// "payload"
-			@ApiParam(value = "The resource to add", required=true)
 			@RequestBody
 					MyResource payload) {
 
@@ -152,22 +110,12 @@ public class PathRestControllerImpl implements PathRestController {
 		return new ResponseEntity(null, getHeaders(), HttpStatus.CREATED);
 	}
 
-	@Override
 	@RequestMapping(value="/path/to/resources/{resource_id}",method=RequestMethod.PUT,consumes={ "application/json" } , produces={ "application/json" })
-	@ApiOperation(value = "Modify a resource", tags = { "PathsAndMethodsAvailable" }, notes = "Modify the resource with the given id ")
-	@ApiResponses(value = {
-			@ApiResponse(code = 204, message = "Resource modified!"),
-			@ApiResponse(code = 400, message = "Wrong payload"),
-			@ApiResponse(code = 404, message = "Resource not found"),
-			@ApiResponse(code = 500, message = "Unexpected error")
-	})
 	public ResponseEntity updateResource(
 			// "resourceId"
-			@ApiParam(value = "The resource id.", required=true )
 			@PathVariable("resource_id")
 					String resourceId,
 			// "payload"
-			@ApiParam(value = "The resource to add", required=true)
 			@RequestBody
 					MyResource payload) {
 
